d421487197342971e8808a310cbb473a13e87400
BangyStudios <bangystudios@gmail.com>
2022-07-11 21:52:13 +1000

    Created base Trader class.

--- Trader.java
+++ Trader.java
@@ -1,5 +1,20 @@
+enum Currency{BTC};
+enum Exchange{Coinbase};
+
 public class Trader {
+    private String dataFile;
+    private int threshold;
+
+    /**
+     * Constructor for Trader
+     * @param dataFile Path to dataFile for testing different values
+     * @param threshold Percentage threshold
+     */
+    public Trader(String dataFile, int threshold) {
+        this.dataFile = dataFile;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Test\n");
+
     }
 }
